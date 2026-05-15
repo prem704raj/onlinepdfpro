46797790d7e8667be82f001852ad4ff81b48af70
Prem736raj <prem736raj@users.noreply.github.com>
2026-05-15 20:51:50 +0530

    Bump cache for logo update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // OnlinePDFPro Service Worker
 // Modern PWA support with reliable caching strategy
 
-const CACHE_NAME = 'onlinepdfpro-cache-v72'; 
+const CACHE_NAME = 'onlinepdfpro-cache-v73'; 
 const STATIC_ASSETS = [
     'about.html',
     'blog.html',
